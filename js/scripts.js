f8396d2d61f4a2478f0cf40f46075f406b86672d
kostasio1313 <kostasio1313@gmail.com>
2019-02-18 20:13:11 +0200

    Bootstrap JQuery

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+});
